c3fc1548251b479f24633c4d2e1842e4d7b1c1cb
Vitalii <vetalpro30@gmail.com>
2024-01-26 19:30:12 +0200

    Update 13 | posts show

--- app/Http/Controllers/Main/IndexController.php
+++ app/Http/Controllers/Main/IndexController.php
@@ -10,10 +10,6 @@ class IndexController extends Controller
 {
     public function __invoke()
     {
-        $posts = Post::paginate(6);
-        $random_posts = Post::get()->random(4);
-        $most_liked_posts = Post::withCount('mostLikedPost')->orderBy('most_liked_post_count', 'DESC')->get()->take(4);
-//        dd($most_liked_posts);
-       return view('main.index', compact('posts','random_posts', 'most_liked_posts'));
+        return redirect()->route('post.index');
     }
 }
---
+++ app/Http/Controllers/Post/IndexController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers\Post;
+
+use App\Http\Controllers\Controller;
+use App\Models\Post;
+use Illuminate\Http\Request;
+
+class IndexController extends Controller
+{
+    public function __invoke()
+    {
+        $posts = Post::paginate(6);
+        $random_posts = Post::get()->random(4);
+        $most_liked_posts = Post::withCount('mostLikedPost')->orderBy('most_liked_post_count', 'DESC')->get()->take(4);
+       return view('post.index', compact('posts','random_posts', 'most_liked_posts'));
+    }
+}
